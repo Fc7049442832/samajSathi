55bb38337baab69525748fcde0182e0df763fdb9
Fc7049442832 <alokrewa1998@gmail.com>
2024-12-22 11:10:25 +0530

    setup project samajSathi

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<div class="row ">
+    <div class="col-md-1 col-2 text-center">
+        <img src="{{ asset('images/ganeshJi.jpg')}}" alt="" class="header-image">
+    </div>
+    <div class="col-md-11 col-9">
+        <h1>SAMAJ SATHI</h1>
+    </div>
+</div>
+
+<style>
+    .header-image{
+       height: 50px;
+       border-radius: 50%;
+    }
+    h1{
+       padding: 10px 0px;
+       margin: 0px;
+    }
+</style>
